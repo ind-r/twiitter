2dcfaac27ffdba1e695cab42efd5cd763c333c86
inder <inderprbhatti@gmail.com>
2025-01-07 23:52:03 +0530

    added bio on the user profile page

--- src/app/(home)/[username]/user-profile.tsx
+++ src/app/(home)/[username]/user-profile.tsx
@@ -1,10 +1,14 @@
 import { getUserInfoByUsername } from "@/actions/users";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function UserProfile({ username }: { username: string }) {
   const user = await getUserInfoByUsername(username);
   let image = "/default-user.png";
-  if (user && user.image && user.image.includes("http")) {
+  if (!user) {
+    redirect("/");
+  }
+  if (user.image && user.image.includes("http")) {
     image = user.image;
   }
   return (
@@ -26,8 +30,9 @@ export default async function UserProfile({ username }: { username: string }) {
           alt="user image"
           className="w-32 h-32 rounded-full -mt-16 border-4"
         />
-        <h1 className="text-2xl font-semibold pt-3">{user?.nickname}</h1>
-        <h1 className="text-gray-600">@{user?.username}</h1>
+        <h1 className="text-2xl font-semibold pt-3">{user.nickname}</h1>
+        <h1 className="text-gray-600">@{user.username}</h1>
+        <h1 className="pt-3">{user.bio}</h1>
       </div>
     </div>
   );
